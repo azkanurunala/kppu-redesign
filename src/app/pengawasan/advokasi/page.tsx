25b18087e829a04e78f1ce9bbbfc8062e8d7d389
Ahsan Alfatih Nurun Ala <ahsanalfatihna@gmail.com>
2025-08-16 08:28:55 +0000

    halaman Advokasi Kebijakan (/pengawasan/advokasi)

--- src/app/pengawasan/advokasi/page.tsx
+++ src/app/pengawasan/advokasi/page.tsx
@@ -45,7 +45,7 @@ export default function AdvokasiPage() {
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8 }}
-                    className="text-5xl md:text-7xl font-bold font-poppins">Advokasi Kebijakan</motion.h1>
+                    className="text-4xl md:text-5xl lg:text-7xl font-bold font-poppins">Advokasi Kebijakan</motion.h1>
                 <motion.p 
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
